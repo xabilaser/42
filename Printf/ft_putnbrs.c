b473a29eb114da26b7413164b9165f64bb518098
Xabi Lasa <xabilaser@users.noreply.github.com>
2021-11-09 15:47:33 +0100

    Update ft_putnbrs.c

--- Printf/ft_putnbrs.c
+++ Printf/ft_putnbrs.c
@@ -18,7 +18,7 @@ int	ft_putunbr(int n)
 
 	count = 0;
 	if (n > 9)
-		count += ft_putnbr((n / 10) + 48);
+		count += ft_putunbr((n / 10) + 48);
 	else
 		count += ft_putdigit((n % 10) + 48);
 	return (count);
