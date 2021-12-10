47b8c5aadae0d5dd9c1b828608b29c3be63429cf
Xabi Lasa <xabilaser@users.noreply.github.com>
2021-12-10 13:37:35 +0100

    Update server.c

--- MiniTalk/server.c
+++ MiniTalk/server.c
@@ -1,45 +1,16 @@
 #include <unistd.h>
 #include <signal.h>
 
-void	*ft_memset(void *b, int c, size_t n)
-{
-	size_t			i;
-
-	i = 0;
-	while (i < n)
-	{
-		((unsigned char*)b)[i] = (unsigned char)c;
-		i++;
-	}
-	return (b);
-}
-
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
 
-void	ft_putnbr_fd(int n, int fd)
-{
-	unsigned int	nb;
-
-	if (n < 0)
-		ft_putchar_fd('-', fd);
-	nb = n < 0 ? n * -1 : n;
-	if (nb >= 10)
-	{
-		ft_putnbr_fd(nb / 10, fd);
-		ft_putchar_fd(nb % 10 + '0', fd);
-	}
-	else
-		ft_putchar_fd(nb + '0', fd);
-}
-
 void	interpreter(int signo)
 {
 	static size_t	i;
-	static int		bit;
-	static char		buf[100];
+	static int	bit;
+	static char	buf[100];
 
 	if (--bit == -1)
 	{
@@ -61,7 +32,7 @@ void	interpreter(int signo)
 	}
 }
 
-int		main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	pid_t pid;
 
