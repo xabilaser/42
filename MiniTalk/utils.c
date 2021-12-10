1ff179244dd0a5d5ba2c9cba77cf518d8084699b
Xabi Lasa <xabilaser@users.noreply.github.com>
2021-12-10 13:35:08 +0100

    Update utils.c

--- MiniTalk/utils.c
+++ MiniTalk/utils.c
@@ -1,5 +1,40 @@
 #include "libft.h"
 
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	count;
+
+	count = 0;
+	while (count < n)
+	{
+		((unsigned char *)s)[count] = (unsigned char)c;
+		count++;
+	}
+	return ((unsigned char *)s);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		ft_putnbr_fd(147483648, fd);
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(n * -1, fd);
+	}
+	else if (n < 10)
+		ft_putchar_fd((char)(n + '0'), fd);
+	else
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd(n % 10 + '0', fd);
+	}
+}
+
 int	ft_isspace(char c)
 {
 	if (c != '\0')
