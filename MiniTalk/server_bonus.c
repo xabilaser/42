d0b9b3c256c3a156f56b600b82fe1b52ab999bca
Xabi Lasa <xabilaser@users.noreply.github.com>
2022-01-12 18:27:48 +0100

    Update server_bonus.c

--- MiniTalk/server_bonus.c
+++ MiniTalk/server_bonus.c
@@ -2,7 +2,24 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "libft/libft.h"
+#include "./libft/libft.h"
+
+static int	send_signal(int clientpid, char c)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((c & (0x01 << bit)) != 0)
+			kill (clientpid, SIGUSR1);
+		else
+			kill (clientpid, SIGUSR2);
+		usleep(100);
+		bit++;
+	}
+	return (0);
+}
 
 void	signal_holder(int signal)
 {
@@ -12,6 +29,8 @@ void	signal_holder(int signal)
 	if (signal == SIGUSR1)
 		i |= (0x01 << bit);
 	bit++;
+	// CREAR VARIABLE STRING Y SEPARAR PID (NUMEROS DEL FINAL 5?). DESPUÉS ESCRIBIR STRING (MENSAJE)
+	//¿METER CARACTER SEPARADOR PARA DIFERENCIAR Y SEPARAR CON SUBSTRING O SPLIT?
 	if (bit == 8)
 	{
 		ft_putchar_fd(i, 1);
@@ -40,5 +59,7 @@ int	main(int argc, char **argv)
 		signal(SIGUSR2, signal_holder);
 		pause ();
 	}
+	// FALTA PROCESAR LA SEÑAL PARA EXTRAER EL PID DEL CLIENTE Y ENVIARLO CON SEND_SIGNAL
+	send_signal(clientpid, );
 	return (0);
 }
