12c6a183992956d01f126bee8e1682f730410656
Xabi Lasa <xabilaser@users.noreply.github.com>
2022-01-12 17:51:19 +0100

    Create client_bonus.c

---
+++ MiniTalk/client_bonus.c
@@ -0,0 +1,48 @@
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "libft/libft.h"
+
+static int	send_signal(int pidnum, char c)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((c & (0x01 << bit)) != 0)
+			kill (pidnum, SIGUSR1);
+		else
+			kill (pidnum, SIGUSR2);
+		usleep(100);
+		bit++;
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	int		servpid;
+	size_t	i;
+
+	i = 0;
+	if (argc == 3)
+	{
+		servpid = ft_atoi(argv[1]);
+		while (argv[2][i] != '\0')
+		{
+			send_signal(servpid, argv[2][i]);
+			i++;
+		}
+		ft_putstr_fd("\033[92mSignal correctly sended\033[0m\n", 1);
+	}
+	else
+	{
+		ft_putstr_fd("\033[97mUse 3 arguments including:\033[0m", 1);
+		ft_putstr_fd("\033[93m./client [PID] [String_to_send]\033[0m\n", 1);
+		exit(EXIT_FAILURE);
+		return (1);
+	}
+	return (0);
+}
