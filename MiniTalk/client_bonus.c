b6993ba3ce15f41b92c2fab1eef0c086baa6e059
Xabi Lasa <xabilaser@users.noreply.github.com>
2022-01-15 12:27:30 +0100

    Update client_bonus.c

--- MiniTalk/client_bonus.c
+++ MiniTalk/client_bonus.c
@@ -15,7 +15,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "libft/libft.h"
-char*	g_checker;
+char	*g_checker;
 
 static int	send_signal(int pidnum, char c)
 {
@@ -90,19 +90,18 @@ void	pid_checker(int pid)
 
 int	main(int argc, char **argv)
 {
-	int		servpid;
-	int		clientpid;
+	int	servpid;
+	int	clientpid;
+	char	*to_send;
 
-	clientpid = getpid();
+	clientpid = getpid() * 100;
 	g_checker = ft_itoa(clientpid);
-	ft_putnbr_fd(clientpid);
+	to_send = (char*)malloc(sizeof(char) * (ft_strlen(g_checker) + ft_strlen(*argv[2]) + 1));
+	to_send = ft_strtrim(g_checker, *argv[2]);
 	if (argc == 3)
 	{
-		argc += 1;
-		servpid = ft_atoi(argv[1]);
-		sig_sender(servpid, *argv[2]);
-		argv[3] = ft_itoa(clientpid);
-		sig_sender(clientpid, *argv[3]);		
+		servpid = ft_atoi(*argv[1]);
+		sig_sender(servpid, to_send);	
 		ft_putstr_fd("\033[92mSignal correctly sended\033[0m\n", 1);
 	}
 	else
@@ -110,8 +109,10 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("\033[97mUse 3 arguments including:\033[0m", 1);
 		ft_putstr_fd("\033[93m./client [PID] [String_to_send]\033[0m\n", 1);
 		free(g_checker);
+		free(to_send);
 		return (1);
 	}
+	free(to_send);
 	pid_checker(clientpid);
 	return (0);
 }
