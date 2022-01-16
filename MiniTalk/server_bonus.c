cafdf8a00c02e6e583be968c17e7c768bb7aab9f
Xabi Lasa <xabilaser@users.noreply.github.com>
2022-01-16 16:18:37 +0100

    Update server_bonus.c

--- MiniTalk/server_bonus.c
+++ MiniTalk/server_bonus.c
@@ -20,6 +20,8 @@ struct sigaction {
     void    (*sa_handler)(int);    addr of signal handler, or SIG_IGN, or SIG_DFL 
     sigset_t    sa_mask;           additional signals to block 
     int    sa_flags;               signal options
+        /* alternate handler */
+    void    (*sa_sigaction)(int, siginfo_t *, void *);
 };
 
 */
