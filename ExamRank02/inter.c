b78fe49e1fd6b621c0ecc49e43cb2e8759be02be
Xabi Lasa <xabilaser@users.noreply.github.com>
2022-01-20 17:42:13 +0100

    Update inter.c

--- ExamRank02/inter.c
+++ ExamRank02/inter.c
@@ -42,3 +42,17 @@ int	main(int argc, char **argv)
 	write(1, "\n", 1);
 	return (0);
 }
+/* Pseudocódigo:
+Función checkeo. Recibe una cadena, un char y una longitud en entero.
+	- Variables: un contador
+	- Bucle para recorrer la cadena hasta la posición dada
+		o Si el carácter de la cadena es igual al carácter, se sale de la función y devuelve 1.
+	-Si sale del bucle devuelve 0.
+Función main con número de argumentos y array de argumentos.
+	- Variables: dos contadores
+	- Se comprueba que entran los tres argumentos (programa y dos cadenas)
+		o Se recorre el segundo string para poner su contador al tamaño de la longitud.
+		o Se recorre el primer string  con el primer contador
+			 Se comprueba si cada carácter del primer String se repite en su mismo string hasta la longitud del primer contador y que el mismo carácter se repite en la segunda String hasta el char anterior al último. Si se repite en la segunda y en la primera no, se imprime.
+	- Se imprime el salto de línea y se abandona el programa con return (0).
+*/
