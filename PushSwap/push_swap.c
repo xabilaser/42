afadd59edb03ce94a5862d4c26fe0f49644f9cc6
Xabi Lasa <xabilaser@users.noreply.github.com>
2022-02-04 12:11:27 +0100

    Update push_swap.c

--- PushSwap/push_swap.c
+++ PushSwap/push_swap.c
@@ -12,14 +12,37 @@
 
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+// main con va_list
+
+int main(int argc, char **argv)
 {
-	t_list	stack_a;
-	t_list	stack_b;
+	t_list  stack_a;
+	t_list  stack_b;
 
 	if (argc < 2)
 		return (NULL);
 	stack_a = NULL;
 	stack_b = NULL;
-	
-}	
+
+}
+
+// main con arrays
+
+int main(int argc, char **argv)
+{
+	int i;
+	int *fst_stack;
+
+	i = 0;
+	if (argc < 2)
+		return (cd_noargs(argc)); // error de less arguments. Pasa el int de args
+	else if (argc == 2)
+ 
+	/* 
+	 Pseudo:
+	 Si hay dos argumentos
+	 	si el segundo arg primer *char no es " atoi y a la pila. El resto también y ordenar
+		else
+		haz split y sacame solo los números (atoi)
+		copia uno a uno a la pila
+	 */
