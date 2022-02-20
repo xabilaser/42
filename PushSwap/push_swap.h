4d313f4e37d1d72986db3577b44af5a6e78eeffe
Xabi Lasa <xabilaser@users.noreply.github.com>
2022-02-20 20:05:25 +0100

    Update push_swap.h

--- PushSwap/push_swap.h
+++ PushSwap/push_swap.h
@@ -17,22 +17,24 @@
 #include <unistd.h>
 #include <stdio.h>
 
-typedef struct s_list
+/*typedef struct s_list
 {
 	int				*content;
 	struct s_list	*next;
-}	t_list;
+}	t_list; */
 
 int     main(int argc, char **argv);
-int	number_sz(char *str, size_t i);
+int	    number_sz(char *str, size_t i);
 int     *rec_stack(char *str, int *stack);
 int     cd_noargs(int num);
 void	chk_numerrors(char *str, int num);
 void    stack_error(void);
 int     stack_size(int *stack);
-int     stack_indexer(int *stack);
+int     **stack_indexer(int *stack);
 int     order_checker(int *stack);
 int     replace_i(int *stack, size_t j, size_t i);
 int     stack_order(int **stack);
+int 	show_results(int moves);
+int 	main_game(int *numbers);
 
 #endif
